fd939c00f9532016485e38d51fa9f311622c2953
pnikulin <pnnikulin@gmail.com>
2023-05-28 20:41:40 +0300

    commit to main

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+def test_item():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+
+    assert item1.price == 10000  # Price test Item1
+    assert item2.price == 20000  # Price test Item2
+
+    Item.pay_rate = 0.8
+    assert item1.apply_discount() == 8000  # Price discount test
+    assert item2.apply_discount() == 16000  # Price discount test
+
+    assert Item.all == []
